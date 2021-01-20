b01b1b794706773218d6f89567a643bb0ddd34fd
purnimakumarr <69529208+purnimakumarr@users.noreply.github.com>
2021-01-20 17:14:23 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -18,6 +18,8 @@ void insert_sort(int arr[], int s)
             j--;
             count++;
         }
+        if (count == 0) //if the while loop doesn't execute at all then, this means that only one comparison was done
+            count=1;
         arr[j + 1] = temp;
         cout << "\nThe no. of comparisons for " << i << " iterations: " << count;
     }
@@ -34,10 +36,9 @@ int main()
         cin >> a[i];
     insert_sort(a, n);
     cout << "\n\nSorted Data Elements:-\n";
-    cout << "--------------------------\n";
+    cout << "----------------------\n";
     for (i = 0; i < n; i++)
         cout << a[i] << endl;
 
     return 0;
 }
-
